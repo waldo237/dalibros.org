72b689cceedc4289c149b45e23ecf6606b2451ad
waldo237 <waldo23792@gmail.com>
2021-04-29 07:11:36 -0400

    set up the templates to start working

--- bookstore/views.py
+++ bookstore/views.py
@@ -5,5 +5,6 @@ from .models import Donation, Book, Author, Donor, Receiver, Gender
 
 # show description
 
-def index (request):
-    return render(request='store/index.html')
+
+def index(request):
+    return render(request, 'store/index.html')
